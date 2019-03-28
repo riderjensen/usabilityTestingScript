e83aeb6f69102c948fc430a15da285d83d3745f5
rider <Riderjensen@gmail.com>
2019-03-28 15:23:01 -0600

    remove unneeded items from data model

--- models/websiteStorage.model.js
+++ models/websiteStorage.model.js
@@ -4,14 +4,10 @@ const {
 } = mongoose;
 
 const websiteStorage = new Schema({
-	webURL: String,
 	testArray: Array,
 	createdAt: {
 		type: Date,
 		default: Date.now
 	},
-	testName: String,
-	questionArray: Array,
-	shortURL: String
 });
 module.exports = mongoose.model("webStorage", websiteStorage);
