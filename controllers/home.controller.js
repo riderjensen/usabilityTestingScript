136758b6e0599b77b11c4ff979c8002f074d69f8
Rider Jensen <riderjensen@gmail.com>
2019-03-29 09:54:57 -0600

    change home message to placehodler where instructions go

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,16 +1,16 @@
 const WebsiteStorageModel = require('../models/websiteStorage.model');
 
 exports.index = (req, res, next) => {
-    res.send('Home area, generate script tag')
+	res.send('Add documentation here for setting things up')
 }
 
 exports.generate = (req, res, next) => {
-    const newWeb = new WebsiteStorageModel();
-    newWeb.save().then(item => {
-        res.send({
-            url: `${process.env.ADDR}/js?id=${item._id}`
-        });
-    })
-        .catch(err => console.log(err))
+	const newWeb = new WebsiteStorageModel();
+	newWeb.save().then(item => {
+		res.send({
+			url: `${process.env.ADDR}/js?id=${item._id}`
+		});
+	})
+		.catch(err => console.log(err))
 
 }
