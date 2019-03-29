90609da54b0cd2f64ab0136e8febbfc384725615
rider <Riderjensen@gmail.com>
2019-03-28 19:02:39 -0600

    testing and removing logs

--- controllers/replay.controller.js
+++ controllers/replay.controller.js
@@ -1,11 +1,25 @@
+const WebsiteStorageModel = require('../models/websiteStorage.model');
+const UseTrackModel = require('../models/useTrack.model');
+
 exports.findingTests = (req, res, next) => {
     const id = req.params.id;
-    console.log(id);
-    // use model to find associated tests then return JSON with a list of IDs
+    WebsiteStorageModel.findById(id)
+        .then(item => {
+            res.send({
+                "tests": item.testArray
+            })
+        })
+        .catch(err => console.log(err));
 }
 
 exports.findingOneTest = (req, res, next) => {
     const id = req.params.id;
-    console.log(id);
+    UseTrackModel.findById(id)
+        .then(item => {
+            res.send({
+                "item": item
+            })
+        })
+        .catch(err => console.log(err));
     // pull one ID and return something that replays the information
 }
