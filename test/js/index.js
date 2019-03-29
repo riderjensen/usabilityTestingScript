fd51c903e106f37fc3ce31ec575f9ec7f435714a
Rider Jensen <riderjensen@gmail.com>
2019-03-29 09:39:29 -0600

    adding call in index to the API

--- test/js/index.js
+++ test/js/index.js
@@ -1,3 +1,18 @@
-document.getElementById('genScriptTag').addEventListener('click', () => {
-	// fetch call to get script tag
+const genButton = document.getElementById('genScriptTag');
+const genInput = document.getElementById('scriptTag');
+
+genButton.addEventListener('click', () => {
+	fetch('http://localhost:8080', {
+		method: "POST",
+		mode: "cors",
+		cache: "no-cache",
+	}).then(response => response.json())
+		.then(resp => {
+			genInput.value = `<script src="${resp.url}"></script>`;
+			genInput.addEventListener('click', () => {
+				genInput.select();
+				document.execCommand("copy");
+				// show some item saying that the text was copied
+			})
+		})
 })
