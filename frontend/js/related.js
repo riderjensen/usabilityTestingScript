626a0b2a7bfc5cf718ed6fed14c68290842f6a37
Rider Jensen <riderjensen@gmail.com>
2019-04-04 09:02:31 -0600

    Add extra instruction to frontend

--- frontend/js/related.js
+++ frontend/js/related.js
@@ -8,18 +8,22 @@ const returnInfo = document.getElementById('returnInfo');
 genButton.addEventListener('click', () => {
 	// check to make sure item is not empty here THEN
 	fetch(`http://165.22.130.92/replay/${genInput.value}`, {
-			method: "GET",
-			mode: "cors",
-			cache: "no-cache",
-		}).then(response => response.json())
+		method: "GET",
+		mode: "cors",
+		cache: "no-cache",
+	}).then(response => response.json())
 		.then(resp => {
 			// display list of all tests as clickable to http://localhost:8080/replay/findOne/${resp[i]}
 			let textGen = `<p>A total of ${resp.tests.length} tests have been generated. More information can be found here:</p>
-				<textarea style="width: 500px;">
+				<p>
 					<span id="usableID">{YOUR ID HERE}</span>
 					<script src="http://165.22.130.92/js/replay"></script>
-				</textarea>
-			<ul>`;
+				</p>
+			<ul>
+			<p>
+			To view specific information on a test, click one of the below ids. You can view initial information such as browser width and height, client used, and the time the test was taken.
+			</p>
+			`;
 			for (let i = 0; i < resp.tests.length; i++) {
 				textGen += `<li><a href="${OURURL}${resp.tests[i]}">${resp.tests[i]}</a></li>`
 			}
