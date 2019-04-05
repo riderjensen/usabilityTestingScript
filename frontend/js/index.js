b145c305760a0d4920c9165afaa766878a7d2f66
Rider Jensen <riderjensen@gmail.com>
2019-04-05 10:29:15 -0600

    Post question array to the back

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,12 +1,32 @@
 const genButton = document.getElementById('genScriptTag');
 const genInput = document.getElementById('scriptTag');
+const addQuestions = document.getElementById('addQuestions');
+const qustionArea = document.getElementById('questionArea');
 
 genButton.addEventListener('click', () => {
-	fetch('http://165.22.130.92', {
-			method: "POST",
-			mode: "cors",
-			cache: "no-cache",
-		}).then(response => response.json())
+	const questionArray = [];
+	for (let i = 0; i < qustionArea.childNodes.length; i += 2) {
+		qustionArea.childNodes[i].value !== "" ? questionArray.push(qustionArea.childNodes[i].value) : null;
+	}
+
+	let queryItems = {
+		method: "POST",
+		mode: "cors",
+		cache: "no-cache",
+	}
+	const bodyObj = {
+		questionArray: questionArray
+	}
+	questionArray.length > 0 ? queryItems = {
+		method: "POST",
+		mode: "cors",
+		cache: "no-cache",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(bodyObj)
+	} : null;
+	fetch('http://localhost:8080', queryItems).then(response => response.json())
 		.then(resp => {
 			genInput.value = `<script src="${resp.url}" id="usable"></script>`;
 			genInput.addEventListener('click', () => {
@@ -15,4 +35,8 @@ genButton.addEventListener('click', () => {
 				// show some item saying that the text was copied
 			})
 		})
-})
+});
+
+addQuestions.addEventListener('click', () => {
+	document.getElementById('questionArea').innerHTML += '<input class="form-control" value="" /><br />';
+});
