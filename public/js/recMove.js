b9a87519a91de82f2a3ca9ba5c745b24f5c8ce1e
rider <Riderjensen@gmail.com>
2019-04-06 12:57:03 -0600

    Modal now pulls one question and posts

--- public/js/recMove.js
+++ public/js/recMove.js
@@ -1,7 +1,7 @@
 const w = window.innerWidth;
 const h = window.innerHeight;
 const body = document.getElementsByTagName('body')[0];
-const USABLE_URL = 'http://165.22.130.92';
+const USABLE_URL = 'http://localhost:8080';
 
 let x, y;
 let objectArray = [];
@@ -18,11 +18,10 @@ const initInformation = {
 
 postData(`${USABLE_URL}/js`, initInformation)
 	.then(data => {
-		console.log(data)
 		const RETURNED_ID = data.id;
 		const RETURNED_QUESTIONS = data.questions;
 
-		RETURNED_QUESTIONS ? console.log('load question script') : console.log('dont load it');
+		RETURNED_QUESTIONS ? createModalArea(RETURNED_QUESTIONS, RETURNED_ID) : null;
 
 		// these are our event listeners for things we want to track
 		window.addEventListener("scroll", usableScrolling);
@@ -108,12 +107,12 @@ function browser() {
 
 	return browser.prototype._cachedResult =
 		isOpera ? 'Opera' :
-			isFirefox ? 'Firefox' :
-				isSafari ? 'Safari' :
-					isChrome ? 'Chrome' :
-						isIE ? 'IE' :
-							isEdge ? 'Edge' :
-								"Don't know";
+		isFirefox ? 'Firefox' :
+		isSafari ? 'Safari' :
+		isChrome ? 'Chrome' :
+		isIE ? 'IE' :
+		isEdge ? 'Edge' :
+		"Don't know";
 };
 
 // changing the x/y coords anytime that the mouse is moved
@@ -189,4 +188,135 @@ function usableScrolling() {
 		// setting event to object on the scroll event
 		objectArray[objectArray.length - 1].ev = scrollObj;
 	}, 100);
+}
+
+
+
+// ============= MODAL AREA ==============
+
+
+function createModalArea(questionsArray, testId) {
+	createModal();
+
+	const taskList = questionsArray[getRandomInt(0, questionsArray.length)];
+	document.getElementById('taskItem').textContent = taskList;
+
+	// Minimize and Maximize Modal
+	const minMaxButtonEvent = document.getElementById('minMaxButton');
+	minMaxButtonEvent.addEventListener('click', () => {
+		if (minMaxSpan.textContent === '-') {
+			outerDiv.style.cssText = 'width: 500px; height: auto; border: 1px solid #ccc; box-sizing: border-box; border-radius: 10px; position: fixed; bottom: 40px; transition: .3s linear; z-index: 99; left: -502px; background: #fff;';
+			minMaxSpan.textContent = '+';
+		} else {
+			outerDiv.style.cssText = 'width: 500px; height: auto; border: 1px solid #ccc; box-sizing: border-box; border-radius: 10px; position: fixed; bottom: 40px; transition: .3s linear; z-index: 99; left: 0; background: #fff;';
+			minMaxSpan.textContent = '-';
+		}
+	});
+
+	minMaxButtonEvent.onmouseover = () => {
+		minMaxButtonEvent.style.cssText = 'background: cyan; height: 100%; width: 100%; border-radius: 50px; display: flex; justify-content: center; align-items: center; transform: scale(1.1); transition: .2s; cursor: pointer;';
+	};
+
+	minMaxButtonEvent.onmouseleave = () => {
+		minMaxButtonEvent.style.cssText = 'background: cyan; height: 100%; width: 100%; border-radius: 50px; display: flex; justify-content: center; align-items: center; transform: scale(1); transition: .2s;';
+	};
+
+	// Next Button
+	nextButton.addEventListener('mouseover', (e) => {
+		e.target.style.cssText = 'flex-grow: 1; padding: 10px; text-decoration: none; font-family: sans-serif; text-align: center; background: cyan; color: #fff; border-radius: 0 0 10px 0; cursor: pointer;';
+	});
+
+	nextButton.addEventListener('click', () => {
+		let feedbackGet = document.getElementById('usableUserResponseTextArea').value;
+		const questionObj = {
+			question: taskList,
+			answer: feedbackGet,
+			testId: testId
+		};
+		console.log(questionObj);
+		//post feedbackGet to DB
+		postData(`${USABLE_URL}/answerQuestion`, questionObj)
+			.then(data => {
+				console.log('Answer question should be sent to the DB')
+			})
+
+	});
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function createModal() {
+	// Create outer div
+	const outerDiv = document.createElement('div');
+	outerDiv.setAttribute('id', 'taskModal');
+	outerDiv.style.cssText = 'width: 500px; height: auto; border: 1px solid #ccc; box-sizing: border-box; border-radius: 10px; position: fixed; bottom: 40px; transition: .3s linear; z-index: 99; background: #fff; box-shadow: 1px 1px 2px #ccc';
+
+	// Create header
+	const usableLogoDiv = document.createElement('div');
+	const usableLogo = document.createElement('img');
+	usableLogo.setAttribute('src', 'https://agitated-fermi-28bdc8.netlify.com/img/usable_logo.f82721b1.svg');
+	usableLogo.setAttribute('alt', 'Usable Logo');
+	usableLogoDiv.appendChild(usableLogo);
+	usableLogoDiv.style.cssText = 'display: flex; justify-content: center; width: 100%;  border-radius: 10px 10px 0 0; background: #000; padding: 10px 0; border: 1px solid #ccc;';
+
+	outerDiv.appendChild(usableLogoDiv);
+
+	// Create Min-Max Div
+	const minMax = document.createElement('div');
+	minMax.style.cssText = 'position: relative; float: right; width: 40px; height: 40px; right: -50px';
+
+	// Create Min-Max Button
+	const minMaxButton = document.createElement('div');
+	minMaxButton.setAttribute('id', 'minMaxButton');
+	minMaxButton.style.cssText = 'background: cyan; height: 100%; width: 100%; border-radius: 50px; display: flex; justify-content: center; align-items: center;';
+
+	minMax.appendChild(minMaxButton);
+
+	// Create Min-Max Span
+	const minMaxSpan = document.createElement('span');
+	minMaxSpan.textContent = '-'
+	minMaxSpan.style.cssText = 'color: #fff; font-size: 2rem; display: inline-block;'
+
+	minMaxButton.appendChild(minMaxSpan);
+
+
+	outerDiv.appendChild(minMax);
+
+	// Create H1
+	const heading1 = document.createElement('h1');
+	heading1.style.cssText = 'font-family: sans-serif; font-size: 21px; text-transform: uppercase; font-weight: 600; color: #333; margin-bottom: 15px; text-decoration: underline; padding-top: 16px; text-align: center; position: relative; right: -20px';
+	outerDiv.appendChild(heading1);
+
+	// Create Task Paragraph
+	const taskParagraph = document.createElement('p');
+	taskParagraph.setAttribute('id', 'taskItem');
+	taskParagraph.style.cssText = 'line-height: 21px; padding: 15px; font-family: sans-serif;'
+	outerDiv.appendChild(taskParagraph);
+
+	// Input Box
+	const feedback = document.createElement('textarea');
+	feedback.style.cssText = 'padding: 15px; width: 75%; display: block; margin: 25px auto; border-radius: 3px;'
+	feedback.setAttribute('placeholder', 'Give us some feedback about this task!');
+	feedback.id = "usableUserResponseTextArea"
+	outerDiv.appendChild(feedback);
+
+	// Create Bottom Section
+	const bottomSection = document.createElement('div');
+	bottomSection.style.cssText = 'display: flex; border-top: 1px solid #aaa';
+
+	// Create Next Button
+	const nextButton = document.createElement('div');
+	nextButton.textContent = 'Submit';
+	nextButton.style.cssText = 'flex-grow: 1; padding: 10px; text-decoration: none; font-family: sans-serif; text-align: center; background: cyan; color: #fff; border-radius: 0 0 10px 0;';
+
+	bottomSection.appendChild(nextButton);
+
+	outerDiv.appendChild(bottomSection);
+
+	// Append Entire Modal to Body
+	document.querySelector('body').appendChild(outerDiv);
 }
